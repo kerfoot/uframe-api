2d64462e51737ec736e9970770a49821f6565ffd
John Kerfoot <johnkerfoot@gmail.com>
2015-12-21 10:06:33 -0500

    first commit, take 2

---
+++ README.md
@@ -0,0 +1,4 @@
+#UFrame API
+
+Collection of scripts for interacting directly with the Ocean Observatories Intiative UFrame webservices API
+
---
+++ search_uframe_instruments.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import os
+import sys
+import datetime
+from uframe.api import UFrame
+
+def main(args):
+    '''Return the fully qualified reference designator list for all instruments
+    contained in the specified UFrame instance.
+    
+    A search string (-r SEARCH_STRING) may also be specified to restrict results to those matching
+    all or part of the search string.'''
+    
+    status = 0
+    
+    base_url = args.base_url
+    if not base_url:
+        if args.verbose:
+            sys.stderr.write('No uframe_base specified.  Checking UFRAME_BASE_URL environment variable\n')
+            
+        base_url = os.getenv('UFRAME_BASE_URL')
+        
+    if not base_url:
+        sys.stderr.write('No UFrame instance specified')
+        sys.stderr.flush()
+        return 1
+    
+    # Create a UFrame instance   
+    if args.verbose:
+        sys.stderr.write('Creating UFrame API instance\n')
+         
+    uframe = UFrame(base_url=base_url, timeout=args.timeout)
+    
+    # Fetch the table of contents from UFrame
+    if args.verbose:
+        t0 = datetime.datetime.utcnow()
+        sys.stderr.write('Fetching and creating UFrame table of contents...')
+        
+    uframe.fetch_toc()
+    
+    if args.verbose:
+        t1 = datetime.datetime.utcnow()
+        dt = t1 - t0
+        sys.stderr.write('Complete ({:d} seconds)\n'.format(dt.seconds))
+    
+    if (args.reference_designator):
+        instruments = uframe.search_ref_des(args.reference_designator)
+    else:
+        instruments = uframe.reference_designators
+        
+    if args.json:
+        json.dumps(instruments)
+    else:
+        for instrument in instruments:
+            sys.stdout.write('{:s}\n'.format(instrument))
+    
+    return status
+    
+if __name__ == '__main__':
+
+    arg_parser = argparse.ArgumentParser(description=main.__doc__)
+    arg_parser.add_argument('-r', '--reference_designator',
+        help='Name of the array to fetch')
+    arg_parser.add_argument('-j', '--json',
+        dest='json',
+        action='store_true',
+        help='Return response as json.  Default is ascii text.')
+    arg_parser.add_argument('-b', '--baseurl',
+        dest='base_url',
+        help='Specify an alternate uFrame server URL. Must start with \'http://\'.  Value is taken from the UFRAME_BASE_URL environment variable, if set')
+    arg_parser.add_argument('-t', '--timeout',
+        type=int,
+        default=120,
+        help='Specify the timeout, in seconds (Default is 120 seconds).')
+    arg_parser.add_argument('-v', '--verbose',
+        action='store_true',
+        help='Verbose display')
+
+    parsed_args = arg_parser.parse_args()
+
+    sys.exit(main(parsed_args))
