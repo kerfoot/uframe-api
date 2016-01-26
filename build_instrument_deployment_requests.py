5b1ba21a33c71ae049d1b1041827c67d7f278a62
John Kerfoot <kerfoot@marine.rutgers.edu>
2016-01-26 15:47:44 -0500

    build requests by deployment info taken from asset management

---
+++ build_instrument_deployment_requests.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+import datetime
+from UFrame import UFrame
+
+def main(args):
+    '''Return the list of request urls that conform to the UFrame API for the 
+        partial or fully-qualified reference_designator and all telemetry types. 
+        The start and end times for each query are set to the deployment start
+        and end times taken from UFrame's asset management web-service API. 
+        The URLs request all stream L0, L1 and L2 dataset parameters over the entire 
+        time-coverage.  The urls are printed to STDOUT.
+    '''
+    
+    status = 0
+    
+    base_url = args.base_url
+    if not base_url:
+        if args.verbose:
+            sys.stderr.write('No uframe_base specified.  Checking UFRAME_BASE_URL environment variable\n')
+            
+        base_url = os.getenv('UFRAME_BASE_URL')
+        
+    if not base_url:
+        sys.stderr.write('No UFrame instance specified')
+        sys.stderr.flush()
+        return 1
+    
+    # Create a UFrame instance   
+    if args.verbose:
+        sys.stderr.write('Creating UFrame API instance\n')
+         
+    uframe = UFrame(base_url=base_url,
+        timeout=args.timeout,
+        validate=args.validate_uframe)
+    
+    # Fetch the table of contents from UFrame
+    if args.verbose:
+        t0 = datetime.datetime.utcnow()
+        sys.stderr.write('Fetching and creating UFrame table of contents...')
+        
+    # Fetch the UFrame table of contents
+    uframe.fetch_toc()
+    # Fetch the UFrame events
+    uframe.fetch_events()
+    
+    # Set args.tense to None if not either 'past' or 'present'
+    if args.tense.lower() not in ['past', 'present']:
+        args.tense = None
+        
+    if args.verbose:
+        t1 = datetime.datetime.utcnow()
+        dt = t1 - t0
+        sys.stderr.write('Complete ({:d} seconds)\n'.format(dt.seconds))
+    
+    if (args.reference_designator):
+        instruments = uframe.search_instruments(args.reference_designator)
+    else:
+        instruments = uframe.instruments
+        
+    if not instruments:
+        sys.stderr.write('No instruments found for reference designator: {:s}\n'.format(args.reference_designator))
+        sys.stderr.flush()
+    
+    urls = []    
+    for instrument in instruments:
+        
+        request_urls = uframe.instrument_to_deployment_query(instrument,
+            deployment_number = args.deployment_number,
+            tense = args.tense,
+            telemetry=args.telemetry,
+            time_check=args.time_check,
+            exec_dpa=args.no_dpa,
+            application_type=args.format,
+            provenance=args.no_provenance,
+            limit=args.limit,
+            annotations=args.no_annotations,
+            user=args.user,
+            email=args.email)
+    
+        for request_url in request_urls:
+            urls.append(request_url)
+            
+    for url in urls:
+        sys.stdout.write('{:s}\n'.format(url))
+        
+    return status
+    
+if __name__ == '__main__':
+
+    arg_parser = argparse.ArgumentParser(description=main.__doc__)
+    arg_parser.add_argument('reference_designator',
+        help='Partial or fully-qualified reference designator identifying one or more instruments')
+    arg_parser.add_argument('--telemetry',
+        help='Restricts urls to the specified telemetry type')
+    arg_parser.add_argument('-d', '--deployment_number',
+        help='Specify a specific deployment number of the instrument')
+    arg_parser.add_argument('--tense',
+        type=str,
+        default='all',
+        help='String specifying past or present deployments <Default:all>')
+    arg_parser.add_argument('--no_time_check',
+        dest='time_check',
+        default=True,
+        action='store_false',
+        help='Do not replace invalid request start and end times with stream metadata values if they fall out of the stream time coverage')
+    arg_parser.add_argument('--no_dpa',
+        action='store_false',
+        default=True,
+        help='Execute all data product algorithms to return L1/L2 parameters <Default:False>')
+    arg_parser.add_argument('--no_provenance',
+        action='store_false',
+        default=True,
+        help='Include provenance information in the data sets <Default:False>')
+    arg_parser.add_argument('-f', '--format',
+        dest='format',
+        default='netcdf',
+        help='Specify the download format (<Default:netcdf> or json)')
+    arg_parser.add_argument('--no_annotations',
+        action='store_false',
+        default=False,
+        help='Include all annotations in the data sets <Default>:False')
+    arg_parser.add_argument('-l', '--limit',
+        type=int,
+        default=-1,
+        help='Integer ranging from -1 to 10000.  <Default:-1> results in a non-decimated dataset')
+    arg_parser.add_argument('-b', '--baseurl',
+        dest='base_url',
+        help='Specify an alternate uFrame server URL. Must start with \'http://\'.  Must be specified if UFRAME_BASE_URL environment variable is not set')
+    arg_parser.add_argument('-t', '--timeout',
+        type=int,
+        default=120,
+        help='Specify the timeout, in seconds <Default:120>')
+    arg_parser.add_argument('-v', '--verbose',
+        action='store_true',
+        help='Verbose display')
+    arg_parser.add_argument('-u', '--user',
+        dest='user',
+        type=str,
+        help='Add a user name to the query')
+    arg_parser.add_argument('--validate_uframe',
+        action='store_true',
+        help='Attempt to validate the UFrame instance <Default:False>')
+    arg_parser.add_argument('--email',
+        dest='email',
+        type=str,
+        help='Add an email address for emailing UFrame responses to the request once sent')
+
+    parsed_args = arg_parser.parse_args()
+
+    sys.exit(main(parsed_args))
